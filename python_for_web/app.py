cad9b1583ecf9f0331b6af41eae1baa6e9d94cdd
victorvisus <victor.vxg@gmail.com>
2026-04-14 09:28:20 +0200

    inicializacion  del repo

---
+++ python_for_web/app.py
@@ -0,0 +1,51 @@
+import os  # importar el módulo del sistema operativo
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")  # este decorador crea la ruta de inicio
+def home():
+    techs = ["HTML", "CSS", "Flask", "Python"]
+    name = "30 días de desafío de programación en Python"
+    return render_template(
+        "home.html", techs=techs, name=name, title="Página principal"
+    )
+
+
+@app.route("/about")
+def about():
+    name = "30 días de desafío de programación en Python"
+    return render_template("about.html", name=name, title="Acerca de nosotros")
+
+
+@app.route("/post")
+def post():
+    name = "Artículos"
+    return render_template("post.html", name=name, title="Artículos")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    old_job = request.form["old_job"]
+    current_job = request.form["current_job"]
+    country = request.form["country"]
+    print(first_name, last_name, old_job, current_job, country)
+    result_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "old_job": old_job,
+        "current_job": current_job,
+        "country": country,
+    }
+    return render_template("result.html", result_data=result_data, title="Resultado")
+
+
+if __name__ == "__main__":
+    # usamos variables de entorno para despliegue
+    # funciona tanto para producción como para desarrollo
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
